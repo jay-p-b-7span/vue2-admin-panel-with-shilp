aff8ca56a0e195c5993afc3da6fdc5901706f377
jay-p-b-7span <jay.p.b@7span.com>
2022-05-08 17:16:01 +0530

    Integrated Shilp VUe

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+// All PLugins
+import "./plugins/index";
+// All Styles
+import "./style/index.scss";
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
---
+++ src/plugins/shilp-vue.js
@@ -0,0 +1,4 @@
+import Vue from "vue";
+import ShilpVue from "shilp-vue";
+
+Vue.use(ShilpVue, {});
