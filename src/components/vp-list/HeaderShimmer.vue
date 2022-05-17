f5cc56166e5531c7173b199254354cbbe0e770dd
jay-p-b-7span <jay.p.b@7span.com>
2022-05-17 09:17:04 +0530

    Updated Shimmer UI

--- src/components/vp-list/HeaderShimmer.vue
+++ src/components/vp-list/HeaderShimmer.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="animate-pulse flex flex--fit flex--nowrap p-2 mx-4 h-12 flex--stretch"
+    class="animate-pulse flex flex--fit flex--nowrap p-2 mx-4 h-12 flex--stretch g-2"
   >
     <div class="bg-grey-lighter rounded w-52"></div>
     <div class="bg-grey-lighter rounded flex-grow"></div>
--- src/components/vp-list/Shimmer.vue
+++ src/components/vp-list/Shimmer.vue
@@ -1,7 +1,11 @@
 <template>
   <div class="animate-pulse">
     <div class="p-4 divide-y">
-      <div v-for="i in 20" :key="`shimmer-${i}`" class="flex mx-2 py-2">
+      <div
+        v-for="i in 20"
+        :key="`shimmer-${i}`"
+        class="flex mx-2 py-2 g-2 flex--fit"
+      >
         <p class="w-24 bg-grey-lighter h-8 rounded"></p>
         <p class="w-96 bg-grey-lighter h-8 rounded"></p>
         <p class="w-48 bg-grey-lighter h-8 rounded"></p>
